0a7ecd33dfc7d0a35e8b91554513543cf324d488
chen <973521624@qq.com>
2023-04-03 12:27:50 +0800

    发送邮件

--- email_proxy/send_email.cpp
+++ email_proxy/send_email.cpp
@@ -1,6 +1,7 @@
 #include "send_email.h"
 #include "ui_send_email.h"
 #include<QtNetwork>
+#include "QByteArray"
 
 send_email::send_email(QWidget *parent) :
     QWidget(parent),
@@ -30,10 +31,37 @@ send_email::send_email(QWidget *parent) :
 }
 
 void send_email::sendEmail(){
-
     port = 25;
-    m_socket->connectToHost("smpt.qq.com", port);
-
+     m_socket->connectToHost("smtp.163.com",25,QTcpSocket::ReadWrite);  //连接163邮箱
+     m_socket->waitForConnected(1000);
+    WaitAndReadData();
+     m_socket->write("helo localhost\r\n");
+    WaitAndReadData();
+     m_socket->write("auth login\r\n");
+    WaitAndReadData();
+     m_socket->write(m_UserName.toBase64()+"\r\n");  //写入用户名
+    WaitAndReadData();
+     m_socket->write(m_Password.toBase64()+"\r\n");  //写入密码
+    WaitAndReadData();
+     m_socket->write("mail from: <"+m_UserName+">\r\n"); //发送的邮箱
+    WaitAndReadData();
+     m_socket->write("rcpt to: <"+sendIp+">\r\n"); //接收的邮箱
+    WaitAndReadData();
+     m_socket->write("data\r\n");  //开始写入
+    WaitAndReadData();
+     m_socket->write("from:<"+m_UserName+">\r\n");  //发送名称
+    WaitAndReadData();
+     m_socket->write("to:<"+sendIp+">");  //接受名称
+    WaitAndReadData();
+     m_socket->write("data\r\n");
+    WaitAndReadData();
+     m_socket->write("Subject:"+s_Title+"\r\n");  //标题
+     m_socket->write("\r\n");
+     m_socket->write(s_Content.append("\r\n")); //内容
+     m_socket->write(".\r\n");
+    WaitAndReadData();
+     m_socket->write("quit\r\n");
+     m_socket->disconnect();
 }
 
 send_email::~send_email()
@@ -41,8 +69,13 @@ send_email::~send_email()
     delete ui;
 }
 
-void send_email::OpenSocket()
+QString send_email::WaitAndReadData()
 {
-    m_socket->connectToHost(hostName, port);
+    m_socket->waitForReadyRead(1000);
+   QString m_ReceiverData =  m_socket->readAll();
+    return m_ReceiverData;
+}
+void send_email::OpenSocket()
 
+{    int     sockfd = { 0 };
 }
--- email_proxy/send_email.h
+++ email_proxy/send_email.h
@@ -20,13 +20,18 @@ public:
     explicit send_email(QWidget *parent = 0);
     ~send_email();
     void sendEmail();
-
+    QString WaitAndReadData();
 private:
     Ui::send_email *ui;
     QTcpSocket *m_socket;
     void OpenSocket();
     QString hostName;
     quint16 port;
+    QByteArray  m_UserName="13728920416@163.com";
+    QByteArray  m_Password = "KBBDYWFXLTOVLQSD";
+    QByteArray  sendIp ="973521624@qq.com";
+    QByteArray  s_Title="test";
+    QByteArray  s_Content="test";
 };
 
 #endif // SEND_EMAIL_H
